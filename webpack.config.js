385860b58fe848765263ee11485243df53ad151c
KreivenWang <kreiven.wang@hotmail.com>
2021-02-09 18:08:24 +0800

    webpack config: entry/output/module/resolve/plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,47 @@
 const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/ui/index.js',
+  mode: 'development', // enabled useful tools for development
+  entry: {
+    utils: './src/utils/index.ts',
+    ui: {
+      import: './src/ui/index.ts',
+      dependOn: 'utils',
+    },
+    server: {
+      import: './src/server/index.ts',
+      dependOn: 'utils',
+    },
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js', // chunk
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include: [path.resolve(__dirname, 'src')],
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        use: 'css-loader',
+      },
+      {
+        test: /\.ts$/,
+        include: [path.resolve(__dirname, 'src')],
+        exclude: [path.resolve(__dirname, 'node_modules')],
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({ template: './src/ui/index.html' }),
+  ],
 };
